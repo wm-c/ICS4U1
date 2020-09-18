2cea282bc92275b4caebed2b888eb28b34d58a4a
wm-c <wcmeathrel@gmail.com>
2020-09-18 09:31:45 -0400

    Added New project changed string

--- StarStrings/src/main/java/starstrings/App.java
+++ StarStrings/src/main/java/starstrings/App.java
@@ -1,86 +1,60 @@
-/*
- * William Meathrel
- * 17/9/2020
- * 
- * A program that puts starts around a string.
- */
 package starstrings;
 
-import java.util.Scanner;
-
-
 public class App {
-	public static boolean end = false;
-    public static String message = "The unusually warm weather makes me think of summer. It is hard to stay focused on mundane work.";
-    public static final int length = 20;
-    public static int pointer = 0;
 
-    public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+	public static String message = "The unusually warm weather makes me think of summer. It is hard to stay focused on mundane work.";
+	
+	public static String[] splitMessage;
 
-		//System.out.print("Enter a string: ");
-		//message = scanner.nextLine();
-		//scanner.close();
+	public static void main(String[] args) {
+		splitMessage = message.split(" ");
 
-        for(int i = 0; i<length + 4; i++){
-            System.out.print("*");
-        }
-
-        System.out.print("\n");
-
-        while(!end){
-
-			String line = getPhrase();
-			
-			
-            System.out.print("* ");
-
-            System.out.print(line);
-
-            System.out.println(" *");
-        }
-
-
-
-        for(int i = 0; i<length + 4; i++){
-            System.out.print("*");
-        }
-        System.out.print("\n");
-
-		
-    }
-
-    public static String getPhrase(){
-
-		String tempMessage;
-
-		if(message.length() - pointer <= length){
-			end = true;
-			tempMessage = message.substring(pointer, message.length());
-		}else{
-
-			tempMessage = message.substring(pointer, clamp(pointer + length, 0, message.length() - 1));
-			int end = tempMessage.lastIndexOf(" ");
-
-			tempMessage = tempMessage.substring(0, end);
-
-			pointer += tempMessage.length();
+		for (int i = 0; i < 23; i++) {
+			System.out.print("*");
 		}
-		
-		
-		if(tempMessage.startsWith(" ")){
-			tempMessage = tempMessage.stripLeading();
+		System.out.println();
+
+		StringBuilder stringBuilder = new StringBuilder("* ");
+
+		int wordLength;
+
+		for (String word : splitMessage) {
+			wordLength = word.length();
+
+			if (stringBuilder.length() + wordLength > 20) {
+				while (stringBuilder.length() < 21) {
+					stringBuilder.append(" ");
+				}
+
+				stringBuilder.append(" *");
+				System.out.println(stringBuilder.toString());
+				stringBuilder.delete(0, stringBuilder.length() - 1);
+				stringBuilder = new StringBuilder("* ");
+			}
+
+			stringBuilder.append(word + " ");
+
 		}
 
-		while(tempMessage.length() != length){
-			tempMessage += " ";
+		if (stringBuilder.length() > 3) {
+			while (stringBuilder.length() < 21) {
+				stringBuilder.append(" ");
+			}
+			stringBuilder.append(" *");
+			System.out.println(stringBuilder.toString());
 		}
 
-		return tempMessage;
-		
-    }
+		for (int i = 0; i < 23; i++) {
+			System.out.print("*");
+		}
+		System.out.println();
 
-    public static int clamp(int clampedNumber, int lowerClamp, int upperClamp){
-        return Math.min(Math.max(clampedNumber, lowerClamp), upperClamp);
-    }
+	}
+
+	public static void printStringArray(String[] stringArray) {
+		for (String string : stringArray) {
+			System.out.printf(" %s,", string);
+		}
+		System.out.println();
+	}
 }
