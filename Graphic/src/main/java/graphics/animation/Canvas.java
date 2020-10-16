b68e2d29af2d59304f87cd4b8f75668fed1da371
wm-c <wcmeathrel@gmail.com>
2020-10-16 08:48:20 -0400

    antialiasing

--- Graphic/src/main/java/graphics/animation/Canvas.java
+++ Graphic/src/main/java/graphics/animation/Canvas.java
@@ -5,12 +5,9 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.Rectangle;
-import java.awt.Stroke;
-import java.awt.geom.AffineTransform;
-import java.awt.BasicStroke;
+import java.awt.RenderingHints;
 
-import javax.swing.JFrame;
+
 import javax.swing.JPanel;
 import javax.swing.Timer;
 import java.awt.Graphics2D;
@@ -35,7 +32,7 @@ public class Canvas extends JPanel implements ActionListener {
 		setBackground(Color.WHITE);
 		setVisible(true);
 		setFocusable(true);
-
+		
 		// Starts timer
 		timer = new Timer(10, this);
 		timer.start();
@@ -45,8 +42,9 @@ public class Canvas extends JPanel implements ActionListener {
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
 
-		// Converts graphics to graphic2d
+		// Converts graphics to graphic2d, enables anti-aliasing
 		Graphics2D g2d = (Graphics2D) g;
+		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
 		// Values for spacing
 
