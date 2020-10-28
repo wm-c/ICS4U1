7ba884aa4f29a06612b6b31fe0a6cc6b32ed5fac
wm-c <wcmeathrel@gmail.com>
2020-10-28 12:53:02 -0400

    Finished terrain assignment

--- Graphic/src/main/java/graphics/Terrain/MainScreen.java
+++ Graphic/src/main/java/graphics/Terrain/MainScreen.java
@@ -1,19 +1,35 @@
 package graphics.Terrain;
 
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
 
 public class MainScreen {
 	
-	Terrain terrain;
+	static Terrain terrain;
+
+	public static void main(String[] args) {
+		// This is for linux 
+		System.setProperty("sun.java2d.opengl", "true");
+
+		// Creates a new mainscreen
+		new MainScreen();
+	}
+
 
 	public MainScreen(){
+		// Creates jframe and new terrain jpanel
 		JFrame window = new JFrame();
 		terrain = new Terrain();
 
-		window.setTitle("My Window");
+		// Sets name
+		window.setTitle("Terrain Generation");
 
+		// Config Window
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setResizable(false);
 
+		// Packs terrain and displays
 		window.add(terrain);
 		window.pack();
 		window.setVisible(true);
